f5bc6c635d536bdc5010749fcce3f2b8be0d26c9
dskott <ds778@cam.ac.uk>
2019-06-25 05:07:46 +0300

    Add twig

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,12 +4,14 @@
     use Symfony\Component\HttpFoundation\Response;
 
     use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-    class PageController {
+    class PageController extends Controller {
         /**
         * @Route("/")
         */
         public function index(){
-            return new Response('<html>hello</html>');
+            return $this->render('pages/index.html.twig');
+            // return new Response('<html>hello</html>');
         }
     }
