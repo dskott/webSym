308df9ea5f72fa591c20e1e2b75bb248dde3834b
dskott <ds778@cam.ac.uk>
2019-06-26 02:09:30 +0300

    Test SSH keys for github

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,9 +2,13 @@
     namespace App\Controller;
 
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    
 
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 
     class PageController extends Controller {
         /**
